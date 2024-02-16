316546e3321abaeb11281cf8267729393e81c18b
Dzaky Abdurahman <m.dzaky.abd123@gmail.com>
2024-02-17 01:02:23 +0700

    <hotfix>:[try with tiny file]

--- components/tiktok.js
+++ components/tiktok.js
@@ -1,25 +1,33 @@
+// import React from 'react'
+// import Head from 'next/head'
+// export default () => (
+//   <div>
+//     <Head>
+//       <script async src="https://www.tiktok.com/embed.js"></script>
+//     </Head>
+//     <blockquote
+//       className="tiktok-embed rounded-2xl overflow-hidden w-full"
+//       cite="https://www.tiktok.com/@shyakinaa"
+//       data-unique-id="shyakinaa"
+//       data-embed-from="embed_page"
+//       data-embed-type="creator"
+//     >
+//       <section>
+//         <a
+//           target="_blank"
+//           href="https://www.tiktok.com/@shyakinaa?refer=creator_embed"
+//         >
+//           @shyakinaa
+//         </a>
+//       </section>
+//     </blockquote>
+//   </div>
+// )
+
 import React from 'react'
-import Head from 'next/head'
-export default () => (
-  <div>
-    <Head>
-      <script async src="https://www.tiktok.com/embed.js"></script>
-    </Head>
-    <blockquote
-      className="tiktok-embed rounded-2xl overflow-hidden w-full"
-      cite="https://www.tiktok.com/@shyakinaa"
-      data-unique-id="shyakinaa"
-      data-embed-from="embed_page"
-      data-embed-type="creator"
-    >
-      <section>
-        <a
-          target="_blank"
-          href="https://www.tiktok.com/@shyakinaa?refer=creator_embed"
-        >
-          @shyakinaa
-        </a>
-      </section>
-    </blockquote>
-  </div>
-)
+
+const tiktok = () => {
+  return <div>tiktok</div>
+}
+
+export default tiktok
